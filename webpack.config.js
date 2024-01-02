78f54875195b06583e6f51b82a8cb3babdd99ab0
Subhasish Sarkar <30510392+SubhasishSarkar@users.noreply.github.com>
2024-01-02 12:31:05 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
        ] 
     },
     externals: {
-        react:"react"
+        "react": {
+          commonjs: "react",
+          commonjs2: "react",
+          amd: "React",
+          root: "React"
+        },
+        "react-dom": {
+          commonjs: "react-dom",
+          commonjs2: "react-dom",
+          amd: "ReactDOM",
+          root: "ReactDOM"
+        }
     }
 }
